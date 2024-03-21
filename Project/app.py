9f6e102cbb60e083e5274df0b70ed88a3373ae10
Salvik-Krishna <salvikknautiyal@gmail.com>
2024-03-21 11:46:24 +0530

    jwt auth working

--- Project/app.py
+++ Project/app.py
@@ -5,11 +5,17 @@ from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.sql import text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from datetime import *
+from psycopg2 import *
+import time
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 app.config['JWT_SECRET_KEY'] = 'your_secret_key_here'  # Set your JWT secret key
 app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "query_string", "json"]
+app.config["JWT_COOKIE_SECURE"] = False
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=300)  # Set token expiration time
 jwt = JWTManager(app)
 
 # SQLAlchemy Configuration
@@ -48,7 +54,6 @@ def insert_user(session, user_data):
     except:
         session.rollback()
         raise
-
 def get_user_by_username(session, username):
     return session.query(User).filter_by(username=username).first()
 
@@ -60,7 +65,9 @@ def mainpage():
     initialize_user_table(session)
     session.close()
     return render_template('mainpage.html')
-
+@app.route('/admin')
+def a():
+    return render_template("admin.html")
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
@@ -79,8 +86,13 @@ def register():
         set_access_cookies(resp, access_token)
         return redirect(url_for('login'))
     return render_template('register.html')
-
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/new_project')
+def np():
+    return render_template("new_project.html")
+@app.route('/project_page')
+def p():
+    return render_template("project_page.html")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
         data = request.form
@@ -88,7 +100,7 @@ def login():
         password = data["password"]
         session = get_session()
         user1 = get_user_by_username(session, username)
-        session.close()
+        session.close()    
         if user1 and check_password_hash(user1.password, password):
             # Generate JWT token
             access_token = create_access_token(identity=username)
@@ -98,7 +110,9 @@ def login():
         else:
             return jsonify({"message": "Invalid username or password"}), 401
     return render_template('login.html')
-
+@app.route('/editing')
+def r():
+    return render_template("editing.html")
 @app.route('/userdetails')
 @jwt_required()  # Protect this route with JWT authentication
 def userdetails():
@@ -113,6 +127,10 @@ def userdetails():
         return render_template('userdetails.html', user_details=user_details)
     else:
         return "User not found"
+@app.route('/linkandcontribution')
+def c():
+    return render_template("linkandcontribution.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
